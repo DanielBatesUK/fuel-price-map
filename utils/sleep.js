aa2abf05025b582b31835a8ecd8889fe0490ea5f
Daniel Bates <email@danielbates.co.uk>
2026-07-15 13:29:50 +0100

    Added reqId and logTime

--- utils/sleep.js
+++ utils/sleep.js
@@ -1,12 +1,12 @@
 // ################################################################################################
 
-// Imports
-// none
+// My Imports
+import logTime from "./log_time.js";
 
 // ################################################################################################
 
-export default function sleep(ms, logOutput = false, reason = "") {
-  if (logOutput) console.log(`Zzzzz for ${ms} milliseconds${reason !== "" ? ` ${reason}` : ""}...`);
+export default function sleep(reqId, ms, logOutput = false, reason = "") {
+  if (logOutput) console.log(`${logTime(reqId)} Zzzzz for ${ms} milliseconds${reason !== "" ? `: ${reason}` : ""}...`);
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
